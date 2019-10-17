076f683d7e59aad47c36938f37a02f1196bf9319
Ayush Dobhal <adobhal93@gmail.com>
2019-10-16 22:37:04 -0500

    Added leetcode 456

---
+++ 456. 132 Problem/stack.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        stack<int> stk;
+        int s2 = INT_MIN;
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            if (nums[i] < s2)
+                return true;
+            
+            while (!stk.empty() && stk.top() < nums[i]) {
+                s2 = stk.top();
+                stk.pop();
+            }
+            stk.push(nums[i]);
+        }
+        return false;
+    }
+};
